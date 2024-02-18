001b43c7b8622029778b9b8fbc751ab80082b2f7
RayyanNafees <nafees.rayyan@gmail.com>
2024-02-19 02:48:24 +0530

    Removed port

--- main.ts
+++ main.ts
@@ -18,10 +18,4 @@ app.onError((err: unknown, c: Context) => {
   return c.text('Internal Server Error', 500)
 })
 
-Deno.serve(
-  {
-    port: +Deno.env.toObject().RAILWAY_TCP_PROXY_PORT || 8080,
-    hostname: Deno.env.toObject().RAILWAY_PUBLIC_DOMAIN || '0.0.0.0',
-  },
-  app.fetch
-)
+Deno.serve(app.fetch)
